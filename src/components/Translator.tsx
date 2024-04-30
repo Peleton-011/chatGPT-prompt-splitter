c1c9ae86b74019c9305f54777a8478f6fd5b8736
Nico Diaz <nicodiazan@gmail.com>
2024-04-30 19:20:05 +0200

    Move translator function to its own file to be used independently

--- src/components/Translator.tsx
+++ src/components/Translator.tsx
@@ -1,42 +1,15 @@
 import { useState } from "react";
-import axios from "axios";
+import translateText from "../translate";
 
 const Translator = () => {
 	const [inputText, setInputText] = useState("");
 	const [targetLanguage, setTargetLanguage] = useState("en");
 	const [translatedText, setTranslatedText] = useState("");
 
-	async function translateText(text: string, target: string ) {
-		const options = {
-			method: "POST",
-			url: "https://microsoft-translator-text.p.rapidapi.com/translate",
-			params: {
-				"api-version": "3.0",
-				"to[0]": target,
-				textType: "plain",
-				profanityAction: "NoAction",
-			},
-			headers: {
-				"content-type": "application/json",
-				"X-RapidAPI-Key":
-					"eeb86a1312msh5854a4f02f2b24dp1efa14jsn77a77b7a3088",
-				"X-RapidAPI-Host": "microsoft-translator-text.p.rapidapi.com",
-			},
-			data: [
-				{
-					Text: text,
-				},
-			],
-		};
-
-		try {
-			const response = await axios.request(options);
-			console.log(response.data);
-			setTranslatedText(response.data[0].translations[0].text);
-		} catch (error) {
-			console.error(error);
-		}
-	}
+    translateText(inputText, targetLanguage).then((text) => {
+        setTranslatedText(text);
+    });
+	
 	return (
 		<div>
 			<h1>Text Translator</h1>
---
+++ src/translate.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+export default async function translateText(text: string, target: string) {
+	const options = {
+		method: "POST",
+		url: "https://microsoft-translator-text.p.rapidapi.com/translate",
+		params: {
+			"api-version": "3.0",
+			"to[0]": target,
+			textType: "plain",
+			profanityAction: "NoAction",
+		},
+		headers: {
+			"content-type": "application/json",
+			"X-RapidAPI-Key":
+				"eeb86a1312msh5854a4f02f2b24dp1efa14jsn77a77b7a3088",
+			"X-RapidAPI-Host": "microsoft-translator-text.p.rapidapi.com",
+		},
+		data: [
+			{
+				Text: text,
+			},
+		],
+	};
+
+	try {
+		const response = await axios.request(options);
+		return response.data[0].translations[0].text;
+	} catch (error) {
+		console.error(error);
+	}
+}
