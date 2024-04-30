32f6c146bb8f7ecfa832ffb55e31dd2a9d213824
Nico Diaz <nicodiazan@gmail.com>
2024-04-30 21:21:59 +0200

    Implement basic prompt translation based on input text

--- src/components/Splitter.tsx
+++ src/components/Splitter.tsx
@@ -1,14 +1,72 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import translate from "../translate";
+import promptsImport from "../assets/prompts.json";
+
+type PromptLanguage = {
+	initialPrompt: string;
+	startPart: string;
+	endPart: string;
+	startFinalPart: string;
+	endFinalPart: string;
+};
+type Prompts = {
+	[key: string]: PromptLanguage;
+};
+
+const prompts: Prompts = promptsImport;
 
 const Splitter = () => {
 	const [text, setText] = useState<string>("");
 	const [chunks, setChunks] = useState<string[]>([]);
-    const [textLanguage, setTextLanguage] = useState<string>("");
+	const [textLanguage, setTextLanguage] = useState<string>("");
 
-    translate.getLanguage(text).then((language) => {
-        setTextLanguage(language);
-    })
+	const [promptList, setPromptList] = useState<PromptLanguage>(prompts.en);
+	console.log(promptList);
+	const [promptLengths, setPromptLengths] = useState<number[]>(
+		Object.values(promptList).map((prompt: string) => prompt.length)
+	);
+
+	translate.getLanguage(text).then((language) => {
+		setTextLanguage(language);
+	});
+
+	useEffect(() => {
+		async function getTranslatePrompts() {
+			return Object.values(promptList).map((prompt: string) =>
+				translate.translateText(prompt, textLanguage)
+			);
+		}
+
+		getTranslatePrompts().then(
+			(translatedPrompts: Promise<PromptLanguage>[]) => {
+				console.log(translatedPrompts);
+				Promise.all(
+					translatedPrompts.map((p) => p.catch((e) => e))
+				).then((results) => {
+					// Check for errors
+					const errors = results.filter(
+						(result) => result instanceof Error
+					);
+					if (errors.length > 0) {
+						// Handle errors
+						console.error("There was an error:", errors);
+					} else {
+						// All promises resolved successfully
+						console.log("All promises resolved:", results);
+					}
+
+					setPromptList({
+						initialPrompt: results[0],
+						startPart: results[1],
+						endPart: results[2],
+						startFinalPart: results[3],
+						endFinalPart: results[4],
+					});
+				});
+			}
+		);
+		console.log(promptList);
+	}, [textLanguage]);
 
 	const splitText = (inputText: string) => {
 		const chunkSize = 15000;
@@ -57,7 +115,7 @@ const Splitter = () => {
 					</div>
 				))}
 			</div>
-            {textLanguage && <p>Language: {textLanguage}</p>}
+			{textLanguage && <p>Language: {textLanguage}</p>}
 		</div>
 	);
 };
